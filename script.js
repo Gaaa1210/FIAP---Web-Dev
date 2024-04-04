6a8a3d2499f3a8862b03145af98452b1603c23d6
Gaaa1210 <gabrielccwanderley1@gmail.com>
2024-04-03 22:44:58 -0300

    Primeira Aula de Java Script

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert("Hello world!")
+// prompt("Digite seu nome")
+// confirm("Você deseja sair")
+// //comenta assim
+// /*assim também
+// eu sou*/ 
+// //console.log é utilizado para encontrar erros
+// console.log("Gabriel")
+// var variavel = "Gabriel Caetano"
+// alert(variavel)
+// console.log("Olá " + variavel + " sejá bem vindo")
+// var nome = prompt("Digite seu nome")
+
+var idade = prompt("Qual a sua idade")
+var nome = prompt("Qual seu nome")
+var profissao = prompt("Qual sua profissão")
+
+console.log("Olá " + nome + " você tem " + idade + " e trabalha com " + profissao) 
